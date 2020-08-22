e364b0568ff1ac5a1bbd55a225bd3e09cec2a4a0
bou3agga <email>
2020-08-22 23:26:20 +0100

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyAUENiIDFAs2xRd1eFDJFvWenKw3W6Dl_E",
+    authDomain: "slack-clone-c7b09.firebaseapp.com",
+    databaseURL: "https://slack-clone-c7b09.firebaseio.com",
+    projectId: "slack-clone-c7b09",
+    storageBucket: "slack-clone-c7b09.appspot.com",
+    messagingSenderId: "484265216516",
+    appId: "1:484265216516:web:c4f31d3733c9ca782d4351",
+    measurementId: "G-8BGPY06VPF"
+};
+const firebaseapp = firebase.initializeApp(firebaseConfig);
