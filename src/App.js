8c51a8b4a67824a03c921f37541b7eec700bb748
bou3agga <email>
2020-08-22 17:02:58 +0100

    adding the header

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    //bem naming convention
+    <div className="app">
+      <h1>sup nigga </h1>
+      <Header />
+      {/*sidebar*/}
+      {/*react-router->chat screen*/}
+    </div>
+  );
+}
+
+export default App;
