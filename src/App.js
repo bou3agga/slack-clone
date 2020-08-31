7405ac1fa777a2d0abb30454d9df6f04d0cbfcca
bou3agga <email>
2020-08-31 21:50:17 +0100

    adding login page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Login from './Login';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 function App() {
+  const [user, setUser] = useState(null);
   return (
     //bem naming convention
 
     <div className="app">
       <Router>
-        <Header />
-        <div className="app_body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>wassup</h1>
-            </Route>
-          </Switch>
-          {/*react-router->chat screen*/}
-        </div>
+        {!user ? (/*<h1>login page</h1>*/<Login />) : (
+          <>
+            <Header />
+            <div className="app_body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>welcome</h1>
+                </Route>
+              </Switch>
+
+            </div>
+          </>
+        )}
+
       </Router>
-    </div>
+    </div >
   );
 }
 
